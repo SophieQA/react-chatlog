9fb4049867f07e0b12727a2ef93b3088986f9f7b
Qiusi Agre <qiusi.agre@icloud.com>
2025-12-15 16:11:47 -0500

    wave1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
+import messages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const first = messages[0];
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry
+          id={first.id}
+          sender={first.sender}
+          body={first.body}
+          timestamp={first.timeStamp}
+        />
+        {/* Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
